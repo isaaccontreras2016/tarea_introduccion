be35ad16d7fad055241f6642686b4168be7b57f0
Isaac Contreras <isaaccontreras2016gmail.com>
2026-07-15 11:42:27 -0500

    Limpia codigo de auth.js y deja version final

--- auth.js
+++ auth.js
@@ -1,39 +1,19 @@
-<<<<<<< HEAD
 // Módulo de Autenticación de Usuarios
 function iniciarSesion(usuario, contrasena) {
     console.log("Iniciando proceso de validación...");
+
     if (usuario === "" || contrasena === "") {
         return "Error: Todos los campos son obligatorios.";
     }
-    if (usuario === "admin1234" && contrasena === "1234") {
+
+    if (usuario === "admin_final" && contrasena === "1234") {
         return "Acceso concedido.";
     } else {
         return "Error: Credenciales incorrectas.";
     }
 }
+
 // --- PRUEBAS EN VIVO ---
-console.log( iniciarSesion("", "") );
-console.log( iniciarSesion("admin", "wrong") );
-console.log( iniciarSesion("admin", "1234") );
-=======
-// Módulo de Autenticación de Usuarios
-function iniciarSesion(usuario, contrasena) {
-    console.log("Iniciando proceso de validación...");
-    if (usuario === "" || contrasena === "") {
-        return "Error: Todos los campos son obligatorios.";
-    }
-    if (usuario === "admin1234" && contrasena === "1234") {
-        return "Acceso concedido.";
-    } else {
-        return "Error: Credenciales incorrectas.";
-    }
-}
->>>>>>> 1fe7dede75950863ff29c33a10eb25aa5c60d029
-
-<<<<<<< HEAD
-   if (usuario === "admin_local" && contrasena === "1234") {
-   =======
-   if (usuario === "admin_web" && contrasena === "1234") {
-   >>>>>>> 137bfa2b... (o números raros)
-
-if (usuario === "admin_final" && contrasena === "1234") {
+console.log( iniciarSesion("", "") );                  // Caso 1: Campos vacíos
+console.log( iniciarSesion("admin", "wrong") );        // Caso 2: Contraseña incorrecta
+console.log( iniciarSesion("admin_final", "1234") );   // Caso 3: Acceso correcto
